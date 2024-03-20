dd5e7c6116104ca650c6d66cf0f8232fbed400ba
fusan <fusanfilm@163.com>
2024-03-20 08:26:23 +0800

    layout component

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,42 @@
 import {useState, useEffect} from 'react';
 import {getAllStudent} from "./client";
 
+import { Breadcrumb, Layout, Menu ,theme} from 'antd';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import './App.css';
+const { Header, Content, Footer, Sider } = Layout;
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
 function App() {
     const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
     const fetchStudents = () =>
         getAllStudent()
             .then(res => res.json())
@@ -18,9 +52,55 @@ function App() {
     if (students.length <= 0){
         return "no data";
     }
-    return students.map((student,index) => {
-        return <p key={index}>{student.id} {student.name}</p>;
-    });
+    return <Layout
+        style={{
+            minHeight: '100vh',
+        }}
+    >
+        <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+            <div className="demo-logo-vertical" />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        </Sider>
+        <Layout>
+            <Header
+                style={{
+                    padding: 0,
+                    background: colorBgContainer,
+                }}
+            />
+            <Content
+                style={{
+                    margin: '0 16px',
+                }}
+            >
+                <Breadcrumb
+                    style={{
+                        margin: '16px 0',
+                    }}
+                >
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                    style={{
+                        padding: 24,
+                        minHeight: 360,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    Bill is a cat.
+                </div>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Ant Design ©{new Date().getFullYear()} Created by Ant UED
+            </Footer>
+        </Layout>
+    </Layout>
 }
 
 export default App;
